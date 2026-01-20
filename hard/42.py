e6b2d93bad3b4e6f38b5ac986307c3722807eea4
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2026-01-19 21:09:45 -0300

    feat: add trapping rain water

---
+++ hard/42.py
@@ -0,0 +1,22 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        n = len(height)
+        l = 0
+        r =  n - 1
+
+        max_l = height[0]
+        max_r = height[n - 1]
+
+        res = 0
+
+        while l < r:
+            if max_l < max_r:
+                l += 1
+                max_l = max(max_l, height[l])
+                res += max_l - height[l]
+            else:
+                r -= 1
+                max_r = max(max_r, height[r])
+                res += max_r - height[r]
+                
+        return res
