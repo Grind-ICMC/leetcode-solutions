4db0ccbde0d83a279d08fb1ba508e111c3a9dd11
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2026-08-15 12:57:18 -0300

    Implement plusOne method to increment digit list

---
+++ easy/66.py
@@ -0,0 +1,29 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        i = 0
+        one = 1
+
+        digits.reverse()
+
+        while one:
+
+            if i < len(digits):
+
+                if digits[i] != 9:
+
+                    digits[i] += 1
+                    one = 0
+
+                else:
+
+                    digits[i] = 0
+
+            else:
+
+                digits.append(1)
+                one = 0
+
+            i += 1
+
+        digits.reverse()
+        return digits
